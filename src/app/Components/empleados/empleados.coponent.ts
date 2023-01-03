cd99b4e6f7cb0ad22a5f032ddba08ed7fa1b3e9f
jeremias vallejos <jeremias.vallejos78@gmail.com>
2023-01-03 16:51:18 -0300

    first commit

---
+++ src/app/Components/empleados/empleados.coponent.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+//decorador del componente
+@Component({
+    selector:"app-empleados",
+    templateUrl: "./empleados.component.html",
+    styleUrls: ["./empleados.component.css"],
+})
+
+
+export class EmpleadosComponent{}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aprenderAngular';
+  saludo = 'hola como estas'
 }
