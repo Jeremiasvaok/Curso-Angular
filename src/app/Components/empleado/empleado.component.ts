8d709435d4c0c76722bb5cfdc64a364ee4a7cbcb
jeremias vallejos <jeremias.vallejos78@gmail.com>
2023-01-16 14:14:51 -0300

    ang

--- src/app/Components/empleado/empleado.component.ts
+++ src/app/Components/empleado/empleado.component.ts
@@ -9,11 +9,11 @@ import { Component } from '@angular/core';
 export class EmpleadoComponent {
  nombre = 'juan';
  apellido = 'vallejos';
- edad = 44;
+ private edad = 44;
  empresa = 'Municipalidad de Ramallo'
 
-//  getEdad(){
-//   return this.edad;
-//  }
+  getEdad(){
+   return this.edad;
+ }
 }
 
