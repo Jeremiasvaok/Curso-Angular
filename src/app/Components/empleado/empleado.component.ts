c235ed0593500738245af9b33e1306c71eae69da
Jeremias Vallejos Gonzalez <jeremias.vallejos78@gmail.com>
2023-01-30 15:53:43 -0300

    Event Binding

--- src/app/Components/empleado/empleado.component.ts
+++ src/app/Components/empleado/empleado.component.ts
@@ -12,5 +12,23 @@ export class EmpleadoComponent {
   urlImg = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'
   texto = 'esto es un texto'
   desabilitar = false
+
+  //Event Binding
+   numero = 0
+
+   Incrementar(){
+    this.numero += 1
+   }
+
+   Decrementar(){
+    if(this.numero > 0){
+      this.numero -= 1
+    }
+   }
+
+   Reiniciar(){
+    this.numero = 0
+   }
+
 }
 
