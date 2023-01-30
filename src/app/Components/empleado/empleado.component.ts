0df32f138d61f73c0e99d6281dd6847a4e0c07f5
Jeremias Vallejos Gonzalez <jeremias.vallejos78@gmail.com>
2023-01-30 15:19:28 -0300

    binding data

--- src/app/Components/empleado/empleado.component.ts
+++ src/app/Components/empleado/empleado.component.ts
@@ -8,33 +8,8 @@ import { EVENT_MANAGER_PLUGINS } from '@angular/platform-browser';
 })
 
 export class EmpleadoComponent {
- nombre = 'juan';
- apellido = 'vallejos';
- edad = 44;
- habilitacionCuadro= false;
- usuarioRegistrado = false;
-empresa = 'Google'
-textoRegistro= 'No hay nadie resgistrado'
 
-getRegistro(){
-  this.usuarioRegistrado=true;
-}
 
-setUsuarioRegistrado(event:Event){
-  //alert('usuario resgistrado')
-//   console.log(event)
-//  this.textoRegistro ='Ya se esta resgistrado'
- (<HTMLInputElement>event.target).value === 'si' ? this.textoRegistro ='Ya esta resgistrado' : this.textoRegistro ='No esta resgistrado'
-  // este event lo transformamos a un elemento html 
-  
-// if((<HTMLInputElement>event.target).value === 'si'){
-//   this.textoRegistro ='ya esta resgistrado'
-// }else{
-//   this.textoRegistro ='no esta resgistrado'
-// }
-}
-
-llamaEmpresa(value:String){}
-
+  urlImg = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'
 }
 
